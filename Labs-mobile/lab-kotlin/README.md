a882ecea42c87b1aeb977b36de4a83ef2805602c
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-05-09 12:05:44 +0100

    terminer

---
+++ Labs-mobile/lab-kotlin/README.md
@@ -0,0 +1,14 @@
+# lab-kotlin
+
+Ce lab est un travail de validation de Module 1 (Principes de base du Kotlin) de l'autoformation 
+[Principes de base d'Android en Kotlin](https://github.com/labs-mobile/autoformation-android-kotlin)  
+
+## Travail à faire 
+
+- Réalisation des classes kotlin du prototype TodoList
+  - TaskEntry
+  - TaskDao
+  - TaskRepository
+- Programme de Test
+- Débugage de programme de test
+
---
+++ Labs-mobile/lab-kotlin/app/src/main/java/prototype/todolist/ProgrammeTest.kt
@@ -0,0 +1,47 @@
+package prototype.todolist
+
+import prototype.todolist.data.TaskDao
+import prototype.todolist.data.TaskEntry
+import prototype.todolist.data.TaskRepository
+
+fun main (){
+// data class
+/*
+    val task1 = TaskEntry(1,"Task 1",1,null)
+    val task2 = TaskEntry(1,"Task 1",1,null)
+    println(task1.equals(task2))
+    println(task1.toString())
+*/
+
+    val taskrepo = TaskRepository()
+    //println(taskrepo.newTask())
+
+    val task1 = TaskEntry(0,"TaskTest1",1,null)
+    //println(task1)
+
+    // Exemple Ajouter
+    taskrepo.save(task1)
+
+    // Exemple Supprimer
+    taskrepo.delete(1)
+
+    // Exemple Modifier
+    println("Give the id of the task you want modify: ")
+    val enteredValue = Integer.valueOf(readLine())
+    val updateTask = taskrepo.findById(enteredValue)
+
+    print("the task title is : ")
+    println(updateTask.title)
+    print("Modify the task here: ")
+    val enteredString = readLine()
+    updateTask.title = "$enteredString"
+
+    println(updateTask)
+    taskrepo.save(updateTask)
+    println(updateTask)
+
+    // Exemple Afficher
+    for(item in taskrepo.getAllTasks() ){
+        println(item.title )
+    }
+}
