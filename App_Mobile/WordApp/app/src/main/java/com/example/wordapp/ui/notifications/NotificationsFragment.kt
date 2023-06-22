b3308b4528ac26c62a6c9ba5132a459791b49150
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-06-22 11:25:41 +0100

    save

--- App_Mobile/WordApp/app/src/main/java/com/example/wordapp/ui/notifications/NotificationsFragment.kt
+++ App_Mobile/WordApp/app/src/main/java/com/example/wordapp/ui/notifications/NotificationsFragment.kt
@@ -11,33 +11,30 @@ import com.example.wordapp.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+private var _binding: FragmentNotificationsBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View {
+    val notificationsViewModel =
+            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(NotificationsViewModel::class.java)
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(
-                NotificationsViewModel::class.java
-            )
+    _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+    val root: View = binding.root
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    val textView: TextView = binding.textNotifications
+    notificationsViewModel.text.observe(viewLifecycleOwner) {
+      textView.text = it
     }
+    return root
+  }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
